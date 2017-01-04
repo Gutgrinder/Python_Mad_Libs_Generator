d668c2374e830c661aaf401231c32b8380485f6e
Gerrit Ohrner <ohrner90@googlemail.com>
2017-01-04 23:16:10 +0100

    Add files via upload

---
+++ Python_Mad_Libs_Generator.py
@@ -0,0 +1,10 @@
+Print('''
+Mad Libs Generator
+by Gerrit Ohrner
+''')
+
+# Ein input für je Nomen, Verb und Adjektiv
+# externe Datei für späteren Satzbau
+# Auf externe Dateien zugreifen
+# Satz mit eingegebenen Variablen und Textstücken aus externen dateien erstellen
+
